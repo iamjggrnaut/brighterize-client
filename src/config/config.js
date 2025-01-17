46bd6de1bf2d19d21157c77de6ba053ed8ffa7d7
iamjggrnaut <rustam.tahamtan@gmail.com>
2025-01-17 19:37:06 +0300

    updates

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,8 @@
-export const URL = 'http://localhost:5000/api'
+// export const URL = 'http://localhost:5000/api'
 
-export const BASE_URL = 'http://localhost:5000'
+// export const BASE_URL = 'http://localhost:5000'
+
+
+export const URL = 'https://brighterize.arcanedevlab.ru//api'
+
+export const BASE_URL = 'https://brighterize.arcanedevlab.ru/'
