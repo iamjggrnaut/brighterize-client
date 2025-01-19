a7b0e2909260033944fb5e6a2ed43cd7af42457c
iamjggrnaut <rustam.tahamtan@gmail.com>
2025-01-19 21:24:48 +0300

    updates

--- src/service/AuthContext.jsx
+++ src/service/AuthContext.jsx
@@ -70,7 +70,7 @@ export const AuthProvider = ({ children }) => {
 
         if (window.Telegram && window.Telegram.WebApp) {
 
-            alert(JSON.stringify(window.Telegram.WebApp.requestWriteAccess()))
+            alert(JSON.stringify(window.Telegram.WebApp))
 
             // window.Telegram.WebApp.ready(() => {
 
